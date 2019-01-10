6ca03a148e2d2e9a3ca28a4e11a8e442d2d8a570
Edwin Ending II <37063900+Jr555@users.noreply.github.com>
2019-01-10 10:37:16 +0800

    Update server1.php

--- edwin/server1.php
+++ edwin/server1.php
@@ -9,14 +9,15 @@
 	$id = 0;
 	$update = false;
 
-	if (isset($_POST['save'])) {
+	if (isset($_POST['save'],$_GET['username'])) {
+		$username = $_GET['username'];
 		$name = $_POST['name'];
 		$address = $_POST['address'];
 		$phone_number = $_POST['phone_number'];
 
-		mysqli_query($db, "INSERT INTO contact (name, address, phone_number) VALUES ('$name', '$address', '$phone_number')"); 
+		mysqli_query($db, "INSERT INTO contact (username, name, address, phone_number) VALUES ('$username','$name', '$address', '$phone_number')"); 
 		$_SESSION['message']; 
-		header('location: welcome.php');
+		header('location: welcome.php?username='.$username);
 	}
 
 
@@ -25,10 +26,16 @@
 		$name = $_POST['name'];
 		$address = $_POST['address'];
 		$phone_number = $_POST['phone_number'];
+		$query = "SELECT username FROM contact WHERE id = $id";
+		$results = mysqli_query($db,$query);
+		if(mysqli_num_rows($results)){
+			while ($row=mysqli_fetch_array($results)) {
+				mysqli_query($db, "UPDATE contact SET name='$name', address='$address' ,phone_number='$phone_number' WHERE id=$id" );
+				$_SESSION['message']; 
+				header('location: welcome.php?username='.$row['username']);
+			}
+		}
 
-		mysqli_query($db, "UPDATE contact SET name='$name', address='$address' ,phone_number='$phone_number' WHERE id=$id" );
-		$_SESSION['message']; 
-		header('location: welcome.php');
 	}
 
 if (isset($_GET['del'])) {
@@ -49,4 +56,19 @@ if (isset($_GET['del'])) {
 	}
 	
 	// ...	
+		if (isset($_GET['edit'])) {
+		$id = $_GET['edit'];
+		$update = true;
+		$record = mysqli_query($db, "SELECT * FROM contact WHERE id=$id");
+
+		if (mysqli_num_rows($record) == 1) {
+			while ($n = mysqli_fetch_array($record)){
+				$username = $n['username'];
+				$name = $n['name'];
+				$address = $n['address'];
+				$phone_number = $n['phone_number'];
+			}
+		}
+
+	}
 ?>
